2eaac2745aeb3bf099fb98f0ff06c556be43b3f0
ChinhPC2 <phamcongchinh.work@gmail.com>
2024-12-10 13:42:27 +0700

    add exercise 2

---
+++ Kernel_Module_Exercise/.modules.order.cmd
@@ -0,0 +1,1 @@
+savedcmd_/home/chinhpc2/Desktop/Linux_Training_ChinhPC2/Linux-Training/Kernel_Module_Exercise/modules.order := {   echo /home/chinhpc2/Desktop/Linux_Training_ChinhPC2/Linux-Training/Kernel_Module_Exercise/module_a.o; :; } > /home/chinhpc2/Desktop/Linux_Training_ChinhPC2/Linux-Training/Kernel_Module_Exercise/modules.order
---
+++ Kernel_Module_Exercise/module_a.c
@@ -0,0 +1,22 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("ChinhPC2");
+MODULE_DESCRIPTION("Kernel Module A exporting variable x");
+
+int x = 1012;
+EXPORT_SYMBOL(x);
+
+static int module_a_init(void) {
+    printk(KERN_INFO "Module A loaded. Exported variable x = %d\n", x);
+    return 0;
+}
+
+static void module_a_exit(void) {
+    printk(KERN_INFO "Module A unloaded\n");
+}
+
+module_init(module_a_init);
+module_exit(module_a_exit);
