d2656358dd629828839936084551d9562b752d11
ChinhPC2 <phamcongchinh.work@gmail.com>
2024-12-11 16:24:56 +0700

    Add Process exercise: task 02

---
+++ Process_Exercise/task_02/a.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid, child_pid;
+
+    pid = fork();
+
+    if (pid < 0)
+    {
+        printf("Error fork.\n");
+        return 1;
+    } else if (pid == 0)
+    {
+        // Replace child process with program B
+        execl("./b", "b", (char *)NULL);
+
+        printf("Error execl.\n");
+        return 1;
+    } else {
+        child_pid = pid;
+        pid_t parent_pid = getpid();
+        printf("Program A: PID = %d, Child PID = %d.\n", parent_pid, child_pid);
+
+        wait(NULL);
+    }
+    
+    return 0;    
+}
---
+++ Process_Exercise/task_02/readme.md
@@ -0,0 +1,15 @@
+# Task 2: Write a program called A that creates child process called B and print PID and PPID.
+
+Run program:
+
+```
+gcc -o a a.c
+gcc -o b b.c
+```
+```
+./a
+```
+
+Result:
+
+![Result](./images/task_02_result.png)
