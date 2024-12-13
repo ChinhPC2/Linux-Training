e87092572acaedf36cd2d8c422d0ff82b7a38490
ChinhPC2 <phamcongchinh.work@gmail.com>
2024-12-13 15:47:38 +0700

    Coding convention

---
+++ Signal_Exercise/Task_01/task_01.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+ * 
+ * ChinhPC2 
+ * Task 1 - Unit 6
+ * 
+******************************************************************************/
+
+#include <stdio.h>
+#include <signal.h>
+
+void block_ctrl_c_signal_handler(int signo) {
+    printf("Ctrl C blocked!\n");
+}
+
+void main() {
+    signal(SIGINT, block_ctrl_c_signal_handler);
+    while(1);
+}
