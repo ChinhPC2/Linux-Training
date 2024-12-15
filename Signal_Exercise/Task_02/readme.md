0aed2ea0ed577a563c1f47e4c6a735359ad3f16c
Chính Phạm Công <chinhphamcong@192.168.1.9.non-exists.ptr.local>
2024-12-15 11:13:25 +0700

    Add task 02 - Signal Exercise

---
+++ Signal_Exercise/Task_02/readme.md
@@ -0,0 +1,16 @@
+### Task 2:  Write a C program to print the signal mask of the current process. 
+Idea: Create Blocked Signal and print to console
+
+#### Run program:
+
+```
+gcc -o task_02 task_02.c
+
+```
+```
+./task_02
+```
+
+#### Result:
+
+![Result](./image/task_02_result.png)
--- Signal_Exercise/Task_02/task_02.c
+++ Signal_Exercise/Task_02/task_02.c
@@ -1,34 +1,57 @@
 /******************************************************************************
  * 
  * ChinhPC2 
- * Task 2 - Unit 6
+ * Task 1 - Unit 6
  * 
 ******************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
-#include <string.h>
+#include <errno.h>
 
 void print_signal_mask() {
-    sigset_t my_sig_set;
-
-    if (sigprocmask(0, NULL, &my_sig_set) == -1)    
-    {
+    sigset_t signal_set;
+    
+    // Retrieve signal
+    if (sigprocmask(SIG_BLOCK, NULL, &signal_set) == -1) {
         perror("sigprocmask");
-        return;
+        exit(EXIT_FAILURE);
     }
 
+    // Print
+    printf("Current signal mask:\n");
 
-    // Print result
-    printf("Block signal:\n");
-    for (int i = 1; i < NSIG; i++) {
-        if (sigismember(&my_sig_set, i)) {
-            printf("Blocked signal: %d - %s", i, strsignal(i));
+    for (int sig = 1; sig < NSIG; ++sig) {
+        if (sigismember(&signal_set, sig)) {
+            printf("Signal %d is blocked\n", sig);
         }
     }
 }
 
-int main () {
+int main() {
+    sigset_t new_set;
+
+    // Creat signal to test
+    if (sigemptyset(&new_set) == -1) {
+        perror("sigemptyset");
+        exit(EXIT_FAILURE);
+    }
+
+    if (sigaddset(&new_set, SIGINT) == -1) { // Block ctrl C
+        perror("sigaddset");
+        exit(EXIT_FAILURE);
+    }
+
+    // Block the signals in the new set
+    if (sigprocmask(SIG_BLOCK, &new_set, NULL) == -1) {
+        perror("sigprocmask");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Blocked Ctrl C for test.\n");
+
+    // Print
     print_signal_mask();
     return 0;
 }
